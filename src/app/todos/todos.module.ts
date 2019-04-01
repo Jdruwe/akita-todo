8045194931fc419e40805c17baa80c3a325cb229
jdruwe <druwe.jeroen@gmail.com>
2019-04-01 22:27:39 +0200

    Working on akita setup

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TodosPageComponent} from './containers/todos-page/todos-page.component';
+import {TodosComponent} from './components/todos/todos.component';
+import {TodoComponent} from './components/todo/todo.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [TodosPageComponent]
+  declarations: [TodosPageComponent, TodosComponent, TodoComponent]
 })
 export class TodosModule {
 }
